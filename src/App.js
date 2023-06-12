191ace1f08747dd33207a0a224f55da93c8f5644
unknown <khandelwalayush621@gmail.com>
2023-06-12 12:32:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './WeatherApp.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const convert = (value) => {
+  return value && (value - 32) * 5/9
+
 }
 
-export default App;
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const fetchWeatherData = async () => {
+    try {
+      const apiKey = 'edce743860b81501734267fae671b946'; 
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div className="weather-app-container">
+      <h1 className="app-title">Weather App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          className="city-input"
+          placeholder="Enter city name"
+          value={city}
+          onChange={handleCityChange}
+        />
+        <button className="get-weather-btn" onClick={fetchWeatherData}>
+          Get Weather
+        </button>
+      </div>
+
+      {weatherData && (
+        <div className="weather-data-container">
+          <h2 className="city-name">Weather in {weatherData.name}</h2>
+          <p className="temperature">
+            Temperature: {Math.round(weatherData?.main?.temp)||0}°C
+          </p>
+          <p className="description">
+            Description: { Array.isArray(weatherData?.weather) && weatherData?.weather[0]?.description}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+export default WeatherApp;
+
+
